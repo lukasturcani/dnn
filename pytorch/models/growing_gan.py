ecd90c6ee52417d0f5cf36f7b944af4857c08629
Lukas Turcani <lukasturcani93@gmail.com>
2018-12-20 19:28:41 +0300

    refactored growing gan

--- pytorch/models/growing_gan.py
+++ pytorch/models/growing_gan.py
@@ -1,83 +1,156 @@
 from torch import nn
 
 
-class GrowingGan:
-    def __init__(self, args):
+class Generator(nn.Module):
+    def __init__(self,
+                 image_channels,
+                 input_channels,
+                 output_channels,
+                 kernel_sizes,
+                 strides,
+                 paddings):
+
+        super().__init__()
         self.phase = 0
-        self.args = args
-        self.generator = nn.Sequential()
-        self.discriminator = nn.Sequential()
+        self.image_channels = image_channels
+
+        params = zip(input_channels,
+                     output_channels,
+                     kernel_sizes,
+                     strides,
+                     paddings)
+
+        self.convs = nn.ModuleList()
+        self.batch_norms = nn.ModuleList()
+        self.activations = nn.ModuleList()
+
+        for i, p in enumerate(params):
+            in_channels, out_channels, kernel_size, stride, padding = p
+
+            conv = nn.ConvTranspose2d(in_channels=in_channels,
+                                      out_channels=out_channels,
+                                      kernel_size=kernel_size,
+                                      stride=stride,
+                                      padding=padding,
+                                      bias=False)
+            self.convs.append(conv)
+
+            batch_norm = nn.BatchNorm2d(num_features=out_channels)
+            self.batch_norms.append(batch_norm)
+
+            self.activations.append(nn.ReLU(inplace=True))
+
+        self.final_activation = nn.Tanh()
 
     def grow(self):
-        self.grow_generator()
-        self.grow_discriminator()
         self.phase += 1
-
-    def grow_generator(self):
-
-        # Change the last activation from tanh to leaky relu.
-        if self.phase:
-            removed = list(self.generator.children())[:-2]
-            self.generator = nn.Sequential(*removed)
-
-        conv = nn.ConvTranspose2d(
-                in_channels=self.args.g_input_channels[self.phase],
-                out_channels=self.args.g_output_channels[self.phase],
-                kernel_size=self.args.g_kernel_sizes[self.phase],
-                stride=self.args.g_strides[self.phase],
-                padding=self.args.g_paddings[self.phase],
-                bias=False)
-        self.generator.add_module(f'conv_{self.phase}', conv)
-
-        num_features = self.args.g_output_channels[self.phase]
-        batch_norm = nn.BatchNorm2d(num_features=num_features)
-        name = f'batch_norm_{self.phase}'
-        self.generator.add_module(name, batch_norm)
-
-        activation = nn.LeakyReLU(self.args.lrelu_alpha)
-        name = f'activation_{self.phase}'
-        self.generator.add_module(name, activation)
-
-        conv = nn.Conv2d(
-                    in_channels=self.args.g_output_channels[self.phase],
-                    out_channels=self.args.image_channels,
+        self.final_conv = nn.Conv2d(
+                    in_channels=self.convs[self.phase-1].out_channels,
+                    out_channels=self.image_channels,
                     kernel_size=1,
                     stride=1,
                     padding=0,
                     bias=False)
-        self.generator.add_module('final_conv', conv)
-        self.generator.add_module('final_activation', nn.Tanh())
 
-    def grow_discriminator(self):
-        # Remove last layer which performs classification.
-        if self.phase:
-            removed = list(self.discriminator.children())[:-1]
-            self.discriminator = nn.Sequential(*removed)
+    def forward(self, x):
 
-        conv = nn.Conv2d(
-                    in_channels=self.args.d_input_channels[self.phase],
-                    out_channels=self.args.d_output_channels[self.phase],
-                    kernel_size=self.args.d_kernel_sizes[self.phase],
-                    stride=self.args.d_strides[self.phase],
-                    padding=self.args.d_paddings[self.phase],
+        for i in range(self.phase):
+            x = self.convs[i](x)
+
+            if i < len(self.batch_norms):
+                x = self.batch_norms[i](x)
+
+            x = self.activations[i](x)
+
+        x = self.final_conv(x)
+        return self.final_activation(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self,
+                 image_channels,
+                 input_channels,
+                 output_channels,
+                 kernel_sizes,
+                 strides,
+                 paddings,
+                 final_kernel_size,
+                 lrelu_alpha):
+
+        super().__init__()
+        self.phase = 0
+        self.image_channels = image_channels
+        self.final_kernel_size = final_kernel_size
+
+        params = zip(input_channels,
+                     output_channels,
+                     kernel_sizes,
+                     strides,
+                     paddings)
+
+        self.convs = nn.ModuleList()
+        self.batch_norms = nn.ModuleList()
+        self.activations = nn.ModuleList()
+        for i, p in enumerate(params):
+            in_channels, out_channels, kernel_size, stride, padding = p
+
+            conv = nn.Conv2d(in_channels=in_channels,
+                             out_channels=out_channels,
+                             kernel_size=kernel_size,
+                             stride=stride,
+                             padding=padding,
+                             bias=False)
+            self.convs.append(conv)
+
+            self.activations.append(nn.LeakyReLU(lrelu_alpha))
+
+            if i != 0:
+                batch_norm = nn.BatchNorm2d(num_features=out_channels)
+                self.batch_norms.append(batch_norm)
+
+    def grow(self):
+        self.phase += 1
+        self.final_conv = nn.Conv2d(
+                    in_channels=self.convs[self.phase-1].out_channels,
+                    out_channels=self.image_channels,
+                    kernel_size=self.final_kernel_size,
+                    stride=1,
+                    padding=0,
                     bias=False)
-        self.discriminator.add_module(f'conv_{self.phase}', conv)
 
-        if self.phase:
-            num_features = self.args.d_output_channels[self.phase]
-            batch_norm = nn.BatchNorm2d(num_features=num_features)
-            name = f'batch_norm_{self.phase}'
-            self.discriminator.add_module(name, batch_norm)
+    def forward(self, x):
 
-        activation = nn.LeakyReLU(self.args.lrelu_alpha)
-        name = f'activation_{self.phase}'
-        self.discriminator.add_module(name, activation)
+        for i in range(self.phase):
+            x = self.convs[i](x)
 
-        conv = nn.Conv2d(
-                in_channels=self.args.d_output_channels[self.phase],
-                out_channels=1,
-                kernel_size=self.args.d_final_kernel_size,
-                stride=1,
-                padding=0,
-                bias=False)
-        self.discriminator.add_module('final_conv', conv)
+            if i != 0:
+                x = self.batch_norms[i-1](x)
+
+            x = self.activations[i](x)
+
+        return self.final_conv(x)
+
+
+class GrowingGan:
+    def __init__(self, args):
+        self.generator = Generator(
+                    image_channels=args.image_channels,
+                    input_channels=args.g_input_channels,
+                    output_channels=args.g_output_channels,
+                    kernel_sizes=args.g_kernel_sizes,
+                    strides=args.g_strides,
+                    paddings=args.g_paddings)
+
+        self.discriminator = Discriminator(
+                    image_channels=args.image_channels,
+                    input_channels=args.d_input_channels,
+                    output_channels=args.d_output_channels,
+                    kernel_sizes=args.d_kernel_sizes,
+                    strides=args.d_strides,
+                    paddings=args.d_paddings,
+                    final_kernel_size=args.d_final_kernel_size,
+                    lrelu_alpha=args.lrelu_alpha)
+
+    def grow(self):
+        self.generator.grow()
+        self.discriminator.grow()
