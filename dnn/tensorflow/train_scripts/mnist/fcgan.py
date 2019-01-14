b4b29fa8bb0d0d367ff0231d565a7b2311661718
Lukas Turcani <lukasturcani93@gmail.com>
2019-01-14 14:19:41 +0000

    fixed up fcgan

--- dnn/tensorflow/train_scripts/mnist/fcgan.py
+++ dnn/tensorflow/train_scripts/mnist/fcgan.py
@@ -5,6 +5,9 @@ import numpy as np
 from sklearn.preprocessing import LabelBinarizer
 import pickle
 import os
+from torchvsion.utils import save_image
+import torch
+import torch.nn.functional as F
 
 from dnn.tensorflow.models.gan.fcgan import model_fn
 
@@ -80,30 +83,114 @@ def get_input_fn(batch_size, images, labels, params):
     return input_fn, init_hook
 
 
+def sample_generator(params):
+    """
+    Uses the generator to create new images and saves them.
+
+    Parameters
+    ----------
+    params : :class:`Namespace`
+        The parameters passed to the script.
+
+    Returns
+    -------
+    None : :class:`NoneType`
+
+    """
+
+    # Create a new input_fn which feeds the generator with data.
+    # A custom one is defined here to avoid loading the MNIST dataset.
+    # Because all we are doing is sampling the generator, we only
+    # need a noise tensor really.
+    def input_fn():
+        noise = tf.random_normal(shape=[params.num_images, 100])
+
+        # This is ignored if --labels is not used. When --labels is
+        # used, the indices argument determines which digits get
+        # sampled.
+        indices = [i % 10 for i in range(params.num_images)]
+        gen_labels = tf.one_hot(indices=indices,
+                                depth=10,
+                                dtype=tf.float32)
+        return {
+            'noise': noise,
+            'gen_labels': gen_labels,
+            # Create a dummy tensor which feeds noise where MNIST
+            # images should normally go. As we are only sampling
+            # the generator, this should never be used but must exist
+            # because the graph defintion requires it.
+            'images': tf.random_normal(
+                                     shape=[params.num_images, 28*28])
+        }
+
+    # Load the estimator on the CPU so that we can sample the generator
+    # while the network is being trained on the GPU.
+    config = tf.estimator.RunConfig(model_dir=params.model_dir,
+                                    device_fn=lambda: 'cpu:/0')
+
+    estimator = tf.estimator.Estimator(model_fn=model_fn,
+                                       model_dir=params.model_dir,
+                                       config=config,
+                                       params=params)
+
+    sample = estimator.predict(input_fn=input_fn,
+                               predict_keys='g_images')
+    images = torch.from_numpy(next(sample)['g_images'])
+    images = F.upsample(images*0.5 + 0.5, scale_factor=10)
+    save_image(images.numpy(), 'generator_sample.jpg')
+
+
 def make_parser():
+    """
+    Creates the command line parser for the script.
+
+    Returns
+    -------
+    :class:`argparse.ArgumentParser`
+        Command line argument parser for the script.
+
+    """
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_dir', default='output')
     parser.add_argument('--batch_size', default=100, type=int)
     parser.add_argument('--train_steps', default=1_000_000, type=int)
     parser.add_argument('--save_step', default=10_000, type=int)
-    parser.add_argument('--learning_rate', default=0.0002, type=float)
+    parser.add_argument('--learning_rate', default=2e-4, type=float)
     parser.add_argument('--lrelu_alpha', default=0.2, type=float)
     parser.add_argument('--label_smoothing', default=0.3, type=float)
-    parser.add_argument('--labels', action='store_true')
+    parser.add_argument('--sample_generator', action='store_true')
     parser.add_argument('--beta1', default=0.5, type=float)
+
+    parser.add_argument('--labels',
+                        action='store_true',
+                        help='Use label information when trainng.')
+
     parser.add_argument('--g_fc_layers',
                         default=[256, 512, 1024],
                         type=int,
                         nargs='+')
+
     parser.add_argument('--d_fc_layers',
                         default=[1024, 512, 256],
                         type=int,
                         nargs='+')
+
+    parser.add_argument('--num_images',
+                        default=20,
+                        type=int,
+                        help=('The number of images to '
+                              'save when using --sample_generator.'))
     return parser
 
 
 def main():
     params = make_parser().parse_args()
+
+    # Check if generation of images requested.
+    if params.sample_generator:
+        sample_generator(params)
+        exit()
 
     ###################################################################
     # Save the hyperparameters.
