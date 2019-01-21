a0dd5fe6a030302259d4b55e654b4dbcb98cbaef
Lukas Turcani <lukasturcani93@gmail.com>
2019-01-21 12:34:32 +0000

    cleaned up pytorch fcgan model code

--- dnn/pytorch/models/gan/fcgan.py
+++ dnn/pytorch/models/gan/fcgan.py
@@ -2,58 +2,104 @@ from torch import nn
 
 
 class Generator(nn.Module):
-    def __init__(self, input_size, fc_layers, lrelu_alpha):
+    """
+    A generator consisting of feed-forward layers.
+
+    The networks uses leaky ReLU activations, except the last layer
+    which is tanh.
+
+    Attributes
+    ----------
+    layers : :class:`torch.Sequential`
+        All the layers of the network.
+
+    """
+
+    def __init__(self, fc_layers, lrelu_alpha):
+        """
+        Initializes the generator.
+
+        Parameters
+        ----------
+        fc_layers : :class:`list` of :class:`int`
+            The number of neurons in each layer, including the input
+            and output layers.
+
+        lrelu_alpha : :class:`float`
+            The alpha value for the leaky ReLU activations units.
+
+        """
+
         super().__init__()
+        num_torch_layers = len(fc_layers) - 1
+        last_torch_layer_index = num_torch_layers - 1
 
         layers = []
-        activations = []
-        in_features = input_size
-        for i, out_features in enumerate(fc_layers):
+        for i in range(num_torch_layers):
+            in_features = fc_layers[i]
+            out_features = fc_layers[i+1]
+
             layer = nn.Linear(in_features=in_features,
                               out_features=out_features)
             layers.append(layer)
-            in_features = out_features
 
             activation = (nn.LeakyReLU(lrelu_alpha) if
-                          i != len(fc_layers) - 1 else
-                          nn.Tanh())
-            activations.append(activation)
+                          i != last_torch_layer_index else nn.Tanh())
+            layers.append(activation)
 
-        self.fc_layers = nn.ModuleList(layers)
-        self.activations = nn.ModuleList(activations)
+        self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
-        for i in range(len(self.fc_layers)):
-            x = self.fc_layers[i](x)
-            x = self.activations[i](x)
-        return x
+        return self.layers(x)
 
 
 class Discriminator(nn.Module):
-    def __init__(self, input_size, fc_layers, lrelu_alpha):
+    """
+    A discriminator consisting of feed-forward layers.
+
+    All layers use the leaky ReLU activation except the output layer,
+    which does not use an activation.
+
+    Attributes
+    ----------
+    layers : :class:`torch.Sequential`
+        All the layers of the network.
+
+    """
+
+    def __init__(self, fc_layers, lrelu_alpha):
+        """
+        Initializes the discriminator.
+
+        Parameters
+        ----------
+        fc_layers : :class:`list` of :class:`int`
+            The number of neurons in each layer, including the input
+            and output layers.
+
+        lrelu_alpha : :class:`float`
+            The alpha value for the leaky ReLU activations units.
+
+        """
+
         super().__init__()
+        num_torch_layers = len(fc_layers) - 1
+        last_torch_layer_index = num_torch_layers - 1
 
         layers = []
-        activations = []
-        in_features = input_size
-        for i, out_features in enumerate(fc_layers):
+        for i in range(num_torch_layers):
+            in_features = fc_layers[i]
+            out_features = fc_layers[i+1]
+
             layer = nn.Linear(in_features=in_features,
                               out_features=out_features)
             layers.append(layer)
-            in_features = out_features
 
-            if i != len(fc_layers) - 1:
+            if i != last_torch_layer_index:
                 activation = nn.LeakyReLU(lrelu_alpha)
-                activations.append(activation)
+                layers.append(activation)
 
-        self.fc_layers = nn.ModuleList(layers)
-        self.activations = nn.ModuleList(activations)
+        self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
-        for i in range(len(self.fc_layers)):
-            x = self.fc_layers[i](x)
-
-            if i < len(self.activations):
-                x = self.activations[i](x)
-
-        return x
+        return self.layers(x)
