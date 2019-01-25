cd67ac1da5b2d5016360d421a9e3835e217b0348
Lukas Turcani <lukasturcani93@gmail.com>
2019-01-25 12:44:14 +0000

    refactored gan training

--- dnn/pytorch/train_scripts/mnist/fcgan.py
+++ dnn/pytorch/train_scripts/mnist/fcgan.py
@@ -3,200 +3,36 @@ import argparse
 import os
 import shutil
 import torch
-from torch import optim, nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-from torchvision.utils import save_image
 
 from dnn.pytorch.models.fcgan import Generator, Discriminator
+from dnn.pytorch.train_scripts.utils.trainers import GANTrainer
 
 logger = logging.getLogger(__name__)
 
 
-def train(args,
-          generator,
-          discriminator,
-          g_optimizer,
-          d_optimizer,
-          train_loader,
-          epoch):
+def data_loaders(args):
+    """
+    Creates MNIST train and test data loaders.
 
-    generator.train()
-    discriminator.train()
-    criterion = nn.BCEWithLogitsLoss()
+    Parameters
+    ----------
+    args : :class:`Namespace`
+        Command line arguments passed to the script, holding model
+        hyperparameters.
 
-    for batch_id, (real_images, _) in enumerate(train_loader):
-        real_images = (real_images - 0.5) / 0.5
-        batch_size = real_images.size()[0]
+    Returns
+    -------
+    :class:`tuple` of :class:`torch.DataLoader`
+        The train and test DataLoaders, respectively.
 
-        # Reshape real images.
-        real_images = real_images.to('cuda')
-        real_images = real_images.view(batch_size, 28*28)
-
-        # Create fake images.
-        noise = torch.randn(batch_size, args.g_input_size, device='cuda')
-        fake_images = generator(noise)
-
-        ###############################################################
-        # Train the discriminator.
-        ###############################################################
-
-        d_optimizer.zero_grad()
-
-        real_logits = discriminator(real_images)
-        fake_logits = discriminator(fake_images)
-
-        real_target = torch.ones(batch_size, device='cuda')
-        real_target -= args.label_smoothing
-        real_target = real_target.view_as(real_logits)
-        d_real_loss = criterion(real_logits, real_target)
-
-        fake_target = torch.zeros(batch_size, device='cuda')
-        fake_target = fake_target.view_as(fake_logits)
-        d_fake_loss = criterion(fake_logits, fake_target)
-
-        d_loss = d_real_loss + d_fake_loss
-        d_loss.backward()
-        d_optimizer.step()
-
-        ###############################################################
-        # Train the generator.
-        ###############################################################
-
-        g_optimizer.zero_grad()
-        noise = torch.randn(batch_size, args.g_input_size, device='cuda')
-
-        fake_images = generator(noise)
-        fake_logits = discriminator(fake_images)
-
-        fake_target = torch.ones(batch_size, device='cuda')
-        fake_target = fake_target.view_as(fake_logits)
-        g_loss = criterion(fake_logits, fake_target)
-
-        g_loss.backward()
-        g_optimizer.step()
-
-        if batch_id % args.log_interval == 0:
-            msg = ('Train Epoch: {} [{}/{} ({:.0f}%)]\t'
-                   'Discriminator Loss: {:.6f}\t'
-                   'Generator Loss: {:.6f}')
-            msg = msg.format(
-                epoch,
-                batch_id * len(real_images),
-                len(train_loader.dataset),
-                100. * batch_id / len(train_loader),
-                d_loss.item(),
-                g_loss.item())
-            logger.info(msg)
-
-
-def save_images(images, epoch, img_dir):
-    filename = os.path.join(img_dir, f'epoch_{epoch}.png')
-    save_image(images, filename)
-
-
-def test(args, generator, discriminator, test_loader, epoch):
-    generator.eval()
-    discriminator.eval()
-    correct = fake_correct = real_correct = 0
-
-    with torch.no_grad():
-        for real_images, _ in test_loader:
-            batch_size = real_images.size()[0]
-            batch_fake_correct = batch_real_correct = 0
-
-            real_images = real_images.to('cuda')
-            real_images = real_images.view(batch_size, 28*28)
-
-            noise = torch.randn(batch_size,
-                                args.g_input_size,
-                                device='cuda')
-            fake_images = generator(noise)
-
-            real_logits = discriminator(real_images)
-            real_predictions = torch.sigmoid(real_logits).round()
-            real_target = torch.ones(batch_size, device='cuda')
-            real_target = real_target.view_as(real_predictions)
-            batch_real_correct = real_predictions.eq(real_target).sum()
-            real_correct += batch_real_correct.item()
-
-            fake_logits = discriminator(fake_images)
-            fake_predictions = torch.sigmoid(fake_logits).round()
-            fake_target = torch.zeros(batch_size, device='cuda')
-            fake_target = fake_target.view_as(fake_predictions)
-            batch_fake_correct = fake_predictions.eq(fake_target).sum()
-            fake_correct += batch_fake_correct.item()
-
-            correct += real_correct + fake_correct
-
-    msg = ('\nTest set: Accuracy: {}/{} ({:.0f}%)'
-           '\tFake correct: {}\tReal correct: {}\n')
-    msg = msg.format(correct,
-                     2*len(test_loader.dataset),
-                     100. * correct / (2*len(test_loader.dataset)),
-                     fake_correct,
-                     real_correct)
-    logger.info(msg)
-
-    noise = torch.randn(20, args.g_input_size, device='cuda')
-    g_images = generator(noise).view(20, 1, 28, 28)
-    save_images(g_images*0.5 + 0.5, epoch, args.img_dir)
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--seed', default=42, type=int)
-    parser.add_argument('--train_batch_size', default=100, type=int)
-    parser.add_argument('--test_batch_size', default=1000, type=int)
-    parser.add_argument('--learning_rate', default=0.0002, type=float)
-    parser.add_argument('--epochs', default=30, type=int)
-    parser.add_argument('--d_fc_layers',
-                        default=[1024, 512, 256, 1],
-                        type=int,
-                        nargs='+')
-    parser.add_argument('--g_input_size', default=100, type=int)
-    parser.add_argument('--g_fc_layers',
-                        default=[256, 512, 1024, 784],
-                        nargs='+',
-                        type=int)
-    parser.add_argument('--lrelu_alpha', default=0.2, type=float)
-    parser.add_argument('--label_smoothing', default=0.3, type=float)
-    parser.add_argument('--momentum', default=0.5, type=float)
-    parser.add_argument('--logging_level',
-                        default=logging.DEBUG,
-                        type=int)
-    parser.add_argument('--log_interval', default=50, type=int)
-    parser.add_argument('--database_root',
-                        default='/home/lukas/databases')
-    parser.add_argument('--img_dir', default='generated_images')
-    args = parser.parse_args()
-
-    if os.path.exists(args.img_dir):
-        shutil.rmtree(args.img_dir)
-    os.mkdir(args.img_dir)
-
-    logging_fmt = ('%(asctime)s - %(levelname)s - '
-                   '%(module)s - %(msg)s')
-    date_fmt = '%d-%m-%Y %H:%M:%S'
-    logging.basicConfig(level=args.logging_level,
-                        format=logging_fmt,
-                        datefmt=date_fmt)
-    torch.manual_seed(args.seed)
-
-    generator = Generator(
-                    input_size=args.g_input_size,
-                    fc_layers=args.g_fc_layers,
-                    lrelu_alpha=args.lrelu_alpha)
-    generator.to('cuda')
-
-    discriminator = Discriminator(
-                        input_size=28*28,
-                        fc_layers=args.d_fc_layers,
-                        lrelu_alpha=args.lrelu_alpha)
-    discriminator.to('cuda')
+    """
 
     transform = transforms.Compose([
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        lambda image: (image - 0.5) * 2,
+        lambda image: image.view(28*28)
     ])
 
     train_mnist = datasets.MNIST(root=args.database_root,
@@ -223,27 +59,112 @@ def main():
                     num_workers=1,
                     pin_memory=True)
 
-    g_optimizer = optim.Adam(generator.parameters(),
-                             lr=args.learning_rate,
-                             betas=(0.5, 0.999))
-    d_optimizer = optim.Adam(discriminator.parameters(),
-                             lr=args.learning_rate,
-                             betas=(0.5, 0.999))
+    return train_loader, test_loader
 
-    for epoch in range(1, args.epochs + 1):
-        train(args=args,
-              generator=generator,
-              discriminator=discriminator,
-              g_optimizer=g_optimizer,
-              d_optimizer=d_optimizer,
-              train_loader=train_loader,
-              epoch=epoch)
 
-        test(args=args,
-             generator=generator,
-             discriminator=discriminator,
-             test_loader=test_loader,
-             epoch=epoch)
+def main():
+
+    ###################################################################
+    # Create the command line arguments.
+    ###################################################################
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', default=42, type=int)
+    parser.add_argument('--train_batch_size', default=100, type=int)
+    parser.add_argument('--test_batch_size', default=1000, type=int)
+    parser.add_argument('--learning_rate', default=0.0002, type=float)
+    parser.add_argument('--beta1', default=0.5, type=float)
+    parser.add_argument('--beta2', default=0.999, type=float)
+    parser.add_argument('--epochs', default=30, type=int)
+    parser.add_argument('--g_input_size', default=100, type=int)
+    parser.add_argument('--lrelu_alpha', default=0.2, type=float)
+    parser.add_argument('--label_smoothing', default=0.3, type=float)
+    parser.add_argument('--momentum', default=0.5, type=float)
+    parser.add_argument('--log_interval', default=50, type=int)
+    parser.add_argument('--img_dir', default='generated_images')
+
+    parser.add_argument('--d_fc_layers',
+                        default=[28*28, 1024, 512, 256, 1],
+                        type=int,
+                        nargs='+')
+
+    parser.add_argument('--g_fc_layers',
+                        default=[256, 512, 1024, 784],
+                        nargs='+',
+                        type=int)
+
+    parser.add_argument('--logging_level',
+                        default=logging.DEBUG,
+                        type=int)
+    parser.add_argument('--database_root',
+                        default='/home/lukas/databases')
+
+    args = parser.parse_args()
+
+    ###################################################################
+    # Set up output directory.
+    ###################################################################
+
+    if os.path.exists(args.img_dir):
+        shutil.rmtree(args.img_dir)
+    os.mkdir(args.img_dir)
+
+    ###################################################################
+    # Set up logging.
+    ###################################################################
+
+    logging_fmt = ('%(asctime)s - %(levelname)s - '
+                   '%(module)s - %(msg)s')
+    date_fmt = '%d-%m-%Y %H:%M:%S'
+    logging.basicConfig(level=args.logging_level,
+                        format=logging_fmt,
+                        datefmt=date_fmt)
+
+    ###################################################################
+    # Set random seed.
+    ###################################################################
+
+    torch.manual_seed(args.seed)
+
+    ###################################################################
+    # Create the generator.
+    ###################################################################
+
+    generator = Generator(
+                    fc_layers=args.g_fc_layers,
+                    lrelu_alpha=args.lrelu_alpha)
+    generator.to('cuda')
+
+    ###################################################################
+    # Create the discriminator.
+    ###################################################################
+
+    discriminator = Discriminator(
+                        fc_layers=args.d_fc_layers,
+                        lrelu_alpha=args.lrelu_alpha)
+    discriminator.to('cuda')
+
+    ###################################################################
+    # Create the data loaders.
+    ###################################################################
+
+    train_loader, test_loader = data_loaders(args)
+
+    ###################################################################
+    # Create the trainer.
+    ###################################################################
+
+    trainer = GANTrainer(generator=generator,
+                         discriminator=discriminator,
+                         args=args)
+
+    ###################################################################
+    # Train.
+    ###################################################################
+
+    for epoch in range(args.epochs):
+        trainer.train(train_loader)
+        trainer.eval(test_loader)
 
 
 if __name__ == '__main__':
---
+++ dnn/pytorch/train_scripts/utils/trainers.py
@@ -0,0 +1,287 @@
+import os
+import logging
+import torch
+from torch import nn, optim
+from torchvision.utils import save_image
+
+logger = logging.getLogger(__name__)
+
+
+class GANTrainer:
+    """
+    Takes care of running the training loop.
+
+    Attributes
+    ----------
+    generator : :class:`torch.Module`
+        The generator network.
+
+    discriminator : :class:`torch.Module`
+        The discriminator network.
+
+    args : :class:`Namespace`
+        A namespace holding various hyperparameters the trainer needs.
+
+    criterion : :class:`torch.Module`
+        The loss function.
+
+    epochs : :class:`int`
+        The total number of epochs the trainer has gone through.
+
+    g_optimizer : :class:`torch.Optimizer`
+        The generator's optimizer.
+
+    d_optimizer : :class:`torch.Optimizer`
+        The discriminator's optimizer.
+
+    """
+
+    def __init__(self, generator, discriminator, args):
+        """
+        Initializes the trainer.
+
+        Parameters
+        ----------
+        generator : :class:`torch.Module`
+            The generator network.
+
+        discriminator : :class:`torch.Module`
+            The discriminator network.
+
+        args : :class:`Namespace`
+            A namespace hodling various hyperparameters the trainer
+            needs.
+
+        """
+
+        self.generator = generator
+        self.discriminator = discriminator
+        self.args = args
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.epochs = 0
+
+        self.g_optimizer = optim.Adam(generator.parameters(),
+                                      lr=args.learning_rate,
+                                      betas=(args.beta1, args.beta2))
+
+        self.d_optimizer = optim.Adam(discriminator.parameters(),
+                                      lr=args.learning_rate,
+                                      betas=(args.beta1, args.beta2))
+
+    def d_train_step(self, batch_size, real_images):
+        """
+        Carries out a single training step on the discriminator.
+
+        Parameters
+        ----------
+        batch_size : :class:`int`
+            The batch size.
+
+        real_images : :class:`torch.Tensor`
+            A batch of real images used for this training step.
+
+        Returns
+        -------
+        None : :class:`NoneType`
+
+        """
+
+        # Use generator to make fake images.
+        noise = torch.randn(batch_size,
+                            self.args.g_input_size,
+                            device='cuda')
+
+        fake_images = self.generator(noise)
+
+        # Reset accumulated discriminator gradient.
+        self.d_optimizer.zero_grad()
+
+        # Get logits.
+        real_logits = self.discriminator(real_images)
+        fake_logits = self.discriminator(fake_images)
+
+        # Get target labels for real images.
+        real_target = torch.ones(batch_size, device='cuda')
+        real_target -= self.args.label_smoothing
+        real_target = real_target.view_as(real_logits)
+
+        # Calculate loss on real iamges.
+        d_real_loss = self.criterion(real_logits, real_target)
+
+        # Get target labels for fake images.
+        fake_target = torch.zeros(batch_size, device='cuda')
+        fake_target = fake_target.view_as(fake_logits)
+
+        # Calculate loss on fake images.
+        d_fake_loss = self.criterion(fake_logits, fake_target)
+
+        # Calculate total loss.
+        d_loss = d_real_loss + d_fake_loss
+        self.d_loss = d_loss.item()
+
+        # Backprop.
+        d_loss.backward()
+        self.d_optimizer.step()
+
+    def g_train_step(self, batch_size):
+        """
+        Carries out a single training step on the generator.
+
+        Parameters
+        ----------
+        batch_size : :class:`int`
+            The batch size used in this training step.
+
+        Returns
+        -------
+        None : :class:`NoneType`
+
+        """
+
+        # Reset accumulated generator gradient.
+        self.g_optimizer.zero_grad()
+
+        # Create fake images.
+        noise = torch.randn(batch_size,
+                            self.args.g_input_size,
+                            device='cuda')
+
+        fake_images = self.generator(noise)
+
+        # Get logits.
+        fake_logits = self.discriminator(fake_images)
+
+        # Get target labels for fake images.
+        fake_target = torch.ones(batch_size, device='cuda')
+        fake_target = fake_target.view_as(fake_logits)
+
+        # Calculate generator loss.
+        g_loss = self.criterion(fake_logits, fake_target)
+        self.g_loss = g_loss.item()
+
+        # Backprop.
+        g_loss.backward()
+        self.g_optimizer.step()
+
+    def train(self, train_loader):
+        """
+        Trains the generator and discriminator for an epochself.
+
+        Parameters
+        ----------
+        train_loader : :class:`DataLoader`
+            A loader which provides the training set.
+
+        Returns
+        -------
+        None : :class:`NoneType`
+
+        """
+
+        self.epochs += 1
+        self.generator.train()
+        self.discriminator.train()
+
+        for batch_id, (real_images, _) in enumerate(train_loader):
+            batch_size = real_images.size()[0]
+            real_images = real_images.to('cuda')
+
+            self.d_train_step(batch_size, real_images)
+            self.g_train_step(batch_size)
+
+            if batch_id % self.args.log_interval == 0:
+                msg = ('Train Epoch: {} [{}/{} ({:.0f}%)]\t'
+                       'Discriminator Loss: {:.6f}\t'
+                       'Generator Loss: {:.6f}')
+                msg = msg.format(
+                                self.epoch,
+                                batch_id * len(real_images),
+                                len(train_loader.dataset),
+                                100. * batch_id / len(train_loader),
+                                self.d_loss,
+                                self.g_loss)
+                logger.info(msg)
+
+    def eval(self, test_loader):
+        """
+        Evaluates the GAN performance on a test set.
+
+        Parameters
+        ----------
+        test_loader : :class:`test_loader`
+            A loader which provides the test set.
+
+        Returns
+        -------
+        None : :class:`NoneType`
+
+        """
+
+        self.generator.eval()
+        self.discriminator.eval()
+
+        # Total correct prediction counts.
+        correct = fake_correct = real_correct = 0
+
+        with torch.no_grad():
+            for real_images, _ in test_loader:
+                real_images = real_images.to('cuda')
+                batch_size = real_images.size()[0]
+
+                # Correct prediction counts in the batch.
+                batch_fake_correct = batch_real_correct = 0
+
+                # Generate fake images.
+                noise = torch.randn(batch_size,
+                                    self.args.g_input_size,
+                                    device='cuda')
+                fake_images = self.generator(noise)
+
+                # Get predictions on real images.
+                real_logits = self.discriminator(real_images)
+                real_predictions = torch.sigmoid(real_logits).round()
+
+                # Get target labels for real images.
+                real_target = torch.ones(batch_size, device='cuda')
+                real_target = real_target.view_as(real_predictions)
+
+                # Check number of correct predictions on real images.
+                batch_real_correct = (real_predictions
+                                      .eq(real_target)
+                                      .sum())
+                real_correct += batch_real_correct.item()
+
+                # Get predictions on fake images.
+                fake_logits = self.discriminator(fake_images)
+                fake_predictions = torch.sigmoid(fake_logits).round()
+
+                # Get target labels for fake images.
+                fake_target = torch.zeros(batch_size, device='cuda')
+                fake_target = fake_target.view_as(fake_predictions)
+
+                # Check number of correct predictions on fake.
+                batch_fake_correct = (fake_predictions
+                                      .eq(fake_target)
+                                      .sum())
+                fake_correct += batch_fake_correct.item()
+
+                # Get total number of correct predictions.
+                correct += real_correct + fake_correct
+
+        # Log results.
+        msg = ('\nTest set: Accuracy: {}/{} ({:.0f}%)'
+               '\tFake correct: {}\tReal correct: {}\n')
+        msg = msg.format(correct,
+                         2*len(test_loader.dataset),
+                         100. * correct / (2*len(test_loader.dataset)),
+                         fake_correct,
+                         real_correct)
+        logger.info(msg)
+
+        # Save some generated images.
+        noise = torch.randn(20, self.args.g_input_size, device='cuda')
+        g_images = self.generator(noise).view(20, 1, 28, 28)
+
+        filename = os.path.join(self.args.img_dir,
+                                f'epoch_{self.epoch}.png')
+        save_image(g_images*0.5 + 0.5, filename)
